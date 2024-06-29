a22663db2258b85185a74fc73f7c65aa5e04b684
ahmed-mgd <ahmedg.abdel@gmail.com>
2024-06-29 16:25:55 -0400

    Implement current weather

--- dom.js
+++ dom.js
@@ -2,8 +2,18 @@ function setLocation(city, region, country) {
   location.textContent = `${city}, ${region} | ${country}`;
 }
 
+function setWeatherDetails(data, celciusMode) {
+  const temp = celciusMode ? `${data.temp_c}\u00B0C` : `${data.temp_c}\u00B0F`;
+  const feel = celciusMode ? data.feelslike_c + "\u00B0C" : data.feelslike_f + "\u00B0F";
+  currentTemp.textContent = temp;
+  currentFeel.textContent = "Feels like: " + feel;
+  currentCondition.textContent = data.condition;
+  currentCIcon.src = data.condition_icon;
+}
+
 export function updateView(data) {
-    setLocation(data.city, data.region, data.country);
+  setLocation(data.city, data.region, data.country);
+  setWeatherDetails(data, true);
 }
 
 export function getQuery() {
@@ -11,10 +21,12 @@ export function getQuery() {
 }
 
 export const submitBtn = document.querySelector("#submit-button");
+export const weatherContent = document.querySelector("#weather");
 
 const queryField = document.querySelector("#query");
 const location = document.querySelector("#location");
 
-submitBtn.addEventListener("click", () => {
-  const query = getQuery();
-});
+const currentTemp = document.querySelector("#temp");
+const currentFeel = document.querySelector("#feel");
+const currentCondition = document.querySelector("#condition");
+const currentCIcon = document.querySelector("#c-icon");
--- main.js
+++ main.js
@@ -2,9 +2,11 @@ import "./style.css";
 import getWeather from "./weather.js";
 import * as dom from "./dom.js";
 
+dom.weatherContent.style.display = "none";
 let weather;
 
 dom.submitBtn.addEventListener("click", () => {
+  dom.weatherContent.style.display = "block";
   const query = dom.getQuery();
   getWeather(query)
     .then(function (data) {
